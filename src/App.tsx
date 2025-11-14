1b87e540485fbaf9ee0362f21ddd736cfe04fc08
de-scientist <gitaumark502@gmail.com>
2025-11-14 12:29:13 +0300

    install axios as a dependency

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,4 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
@@ -8,16 +6,12 @@ function App() {
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
       <h1>Vite + React</h1>
       <div className="card">
+        <input type="text" placeholder='Enter your country'/>
+         <a href="" target="_blank" rel='noopener'>
+          <h2></h2>
+        </a>
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
         </button>
